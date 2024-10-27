6ef43c226fdd6ae7c791cb0e133f003550eb9ea3
Yash Swarnkar <nuttcrits@gmail.com>
2024-10-27 19:23:22 +0530

    Functionality

---
+++ script.js
@@ -0,0 +1,65 @@
+const first = document.getElementById('1');
+const second = document.getElementById('2');
+const third = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const ten = document.getElementById('10');
+const eleven = document.getElementById('11');
+const twelve = document.getElementById('12');
+const thirteen = document.getElementById('13');
+const fourteen = document.getElementById('14');
+const fiveteen = document.getElementById('15');
+const sixteen = document.getElementById('16');
+
+first.addEventListener('click',()=>{
+    window.location.href = "https://www.electronjs.org/";
+})
+second.addEventListener('click',()=>{
+    window.location.href = "https://excalidraw.com/";
+})
+third.addEventListener('click',()=>{
+    window.location.href = "https://kaboomjs.com/";
+})
+four.addEventListener('click',()=>{
+    window.location.href = "https://slowroads.io/";
+})
+five.addEventListener('click',()=>{
+    window.location.href = "https://file.pizza/";
+})
+six.addEventListener('click',()=>{
+    window.location.href = "https://enorme.tv/home";
+})
+seven.addEventListener('click',()=>{
+    window.location.href = "https://fakedetail.com/";
+})
+eight.addEventListener('click',()=>{
+    window.location.href = "https://tinypng.com/";
+})
+nine.addEventListener('click',()=>{
+    window.location.href = "https://theresanaiforthat.com/";
+})
+ten.addEventListener('click',()=>{
+    window.location.href = "https://sweatco.in/";
+})
+eleven.addEventListener('click',()=>{
+    window.location.href = "https://spline.design/";
+})
+twelve.addEventListener('click',()=>{
+    window.location.href = "https://spotifydown.com/";
+})
+thirteen.addEventListener('click',()=>{
+    window.location.href = "https://saveviddit.com/";
+})
+fourteen.addEventListener('click',()=>{
+    window.location.href = "https://www.shazam.com/";
+})
+fiveteen.addEventListener('click',()=>{
+    window.location.href = "https://downloaderto.com/enO1/youtube-4k-downloader";
+})
+sixteen.addEventListener('click',()=>{
+    window.location.href = "https://getintopc.com/";
+})
