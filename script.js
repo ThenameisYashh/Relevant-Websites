3f84f03fcdfec7b6810cec02a51c3478a674f397
Yash Swarnkar <nuttcrits@gmail.com>
2025-01-01 10:12:02 +0530

    Updated

--- script.js
+++ script.js
@@ -2,88 +2,7 @@ const gitdes = document.getElementById('git-des')
 const linkdes = document.getElementById('link-des')
 const github = document.querySelector('.github')
 const linkedin = document.querySelector('.linkedin')
-const first = document.getElementById('1');
-const second = document.getElementById('2');
-const third = document.getElementById('3');
-const four = document.getElementById('4');
-const five = document.getElementById('5');
-const six = document.getElementById('6');
-const seven = document.getElementById('7');
-const eight = document.getElementById('8');
-const nine = document.getElementById('9');
-const ten = document.getElementById('10');
-const eleven = document.getElementById('11');
-const twelve = document.getElementById('12');
-const thirteen = document.getElementById('13');
-const fourteen = document.getElementById('14');
-const fiveteen = document.getElementById('15');
-const sixteen = document.getElementById('16');
-
-
-first.addEventListener('click',()=>{
-    let url1 = "https://www.electronjs.org/";
-    window.open(url1 , '_blank').focus()
-})
-second.addEventListener('click',()=>{
-    let url2 = "https://excalidraw.com/";
-    window.open(url2 , '_blank').focus()
-})
-third.addEventListener('click',()=>{
-    let url3 = "https://kaboomjs.com/";
-    window.open(url3 , '_blank').focus()
-})
-four.addEventListener('click',()=>{
-    let url4 = "https://slowroads.io/";
-    window.open(url4 , '_blank').focus()
-})
-five.addEventListener('click',()=>{
-    let url5 = "https://file.pizza/";
-    window.open(url5 , '_blank').focus()
-})
-six.addEventListener('click',()=>{
-    let url6 = "https://enorme.tv/home";
-    window.open(url6 , '_blank').focus()
-})
-seven.addEventListener('click',()=>{
-    let url7 = "https://fakedetail.com/";
-    window.open(url7 , '_blank').focus()
-})
-eight.addEventListener('click',()=>{
-    let url8 = "https://tinypng.com/";
-    window.open(url8 , '_blank').focus()
-})
-nine.addEventListener('click',()=>{
-    let url9 = "https://theresanaiforthat.com/";
-    window.open(url9 , '_blank').focus()
-})
-ten.addEventListener('click',()=>{
-    let url10 = "https://sweatco.in/";
-    window.open(url10 , '_blank').focus()
-})
-eleven.addEventListener('click',()=>{
-    let url11 = "https://spline.design/";
-    window.open(url11 , '_blank').focus()
-})
-twelve.addEventListener('click',()=>{
-    let url12 = "https://spotifydown.com/";
-    window.open(url12 , '_blank').focus()
-})
-thirteen.addEventListener('click',()=>{
-    let url13 = "https://saveviddit.com/";
-    window.open(url13 , '_blank').focus()
-})
-fourteen.addEventListener('click',()=>{
-    let url14 = "https://www.shazam.com/";
-    window.open(url14 , '_blank').focus()
-})
-fiveteen.addEventListener('click',()=>{
-    let url15 = "https://downloaderto.com/enO1/youtube-4k-downloader";
-    window.open(url15 , '_blank').focus()
-})
-sixteen.addEventListener('click',()=>{
-    let url16 = "https://getintopc.com/";
-    window.open(url16 , '_blank').focus()    
-})
+const ab = document.querySelector('.all-boxes')
 
 linkedin.addEventListener('click',()=>{
     let url18= "https://www.linkedin.com/in/yash-sonii";
@@ -93,15 +12,15 @@ github.addEventListener('click',()=>{
     let url19= "https://github.com/ThenameisYashh";
     window.open(url19 , '_blank').focus()
 })
-github.addEventListener('mouseover',()=>{
-    gitdes.style.display = 'block';
-})
-github.addEventListener('mouseleave',()=>{
-    gitdes.style.display = 'none';
-})
-linkedin.addEventListener('mouseover',()=>{
-    linkdes.style.display = 'block';
-})
-linkedin.addEventListener('mouseleave',()=>{
-    linkdes.style.display = 'none';
+
+fetch(`https://mocki.io/v1/5ca104b3-dc09-4efe-a8b8-1ad43fa7b338`)
+.then((res)=>res.json())
+.then((data)=>{
+    data.forEach(e => {
+        const boxs = document.createElement('a')
+        boxs.classList.add('boxes-theme')
+        boxs.href = `${e.link}`
+        boxs.innerHTML = `<img src="${e.banner}" alt=""></img><div class="des">${e.description}</div>`
+        ab.append(boxs)
+    });
 })
